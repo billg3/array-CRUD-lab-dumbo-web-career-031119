46b919b33bac5c85a0cee71c93966b7e4f32d0a2
Bill G <gbilly3@gmail.com>
2019-02-21 19:09:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,14 @@
 def create_an_empty_array
-  
+ [] 
 end
 
 def create_an_array
-  
+  cats = ["a", "b", "c", "d"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = ["wow", "I", "am", "really", "learning"]
+    array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
